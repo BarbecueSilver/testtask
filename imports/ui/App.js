f9a184ab10b065e1c0a03e1c691fda1c7f0dbc5c
Andreas <a.leister@ymail.com>
2018-06-02 14:52:15 +0200

    Choosing a chatroom and displaying last sent message

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -4,59 +4,110 @@ import { withTracker } from 'meteor/react-meteor-data';
 import { Chats } from '../api/chats.js';
 import Chat from './Chat.js';
 
+import { BrowserRouter } from 'react-router-dom'
+import { Link, Switch, Route } from 'react-router-dom'
+import ReactDOM from 'react-dom';
+
+
 class App extends Component {
     render() {
         return (
-            <div className="container">
-                <header>
-                    <h1>ircChat</h1>
-                </header>
-
-                <ul>
-                    {this.renderChatList()}
-                </ul>
-
-                {this.renderChatDetails()}
-            </div>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Main />
+                </div>
+            </BrowserRouter>
         );
     }
+}
 
-    renderChatList() {
-        return this.props.chats.map((chat) => (
-            <Chat key={chat._id} chat={chat} />
-        ));
-    }
-
-    renderChatDetails() {
+class Header extends Component {
+    render() {
         return (
-            <div>
-                <textarea name="chatHistory"
-                          readOnly={true}/>
-                <input name="messageDraft"
-                       type="text"
-                       onChange={this.updateMessageDraft}/>
-                <button onClick={this.sendMessage}>
-                    Send
-                </button>
-            </div>
-        )
+            <header>
+                <h1>Available Chats</h1>
+                <div>
+                    <ul>
+                        {
+                            Chats.find({}).fetch().map(chat => (
+                                <li key={chat._id}>
+                                    <Link to={`/chats/${chat._id}`}>{chat.title}</Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </header>
+        );
+    }
+}
+
+class Main extends Component {
+    render() {
+        return (
+            <Switch>
+                <Route exact path='/chats' component={NoChatRoom}/>
+                <Route path='/chats/:chatId' component={ChatRoom}/>
+            </Switch>
+        );
+    }
+}
+
+class NoChatRoom extends Component {
+    render() {
+        return <div>Please select a chat.</div>
+    }
+}
+
+class ChatRoom extends Component {
+    getChatId()
+    {
+        return this.props.match.params.chatId;
     }
 
-    updateMessageDraft(event) {
-        this.state={currentMessageDraft: event.target.value};
+    render() {
+        // Arrange
+        let id = this.getChatId();
+        const chat = Chats.findOne({
+            _id: id
+        });
+
+        // Act
+        if (!chat) {
+            return <div>Sorry, but the chat room was not found</div>
+        }
+        else {
+            return (
+                <main>
+                    <div>
+                        <h1>{chat.title}</h1>
+                        <div>
+                            <textarea name="chatHistory"
+                                      value={chat.messages}
+                                      readOnly={true}/>
+                            <input name="messageDraft"
+                                   type="text"
+                                   ref="messageDraft"
+                                   placeholder="Type your message here ..."/>
+                            <button onClick={this.sendMessage.bind(this)}>
+                                Send
+                            </button>
+                        </div>
+                    </div>
+                </main>
+            )
+        }
     }
 
     sendMessage() {
-        var messageToSend = this.state.currentMessageDraft;
+        // Arrange
+        const messageToSend = ReactDOM.findDOMNode(this.refs.messageDraft).value.trim();
+        let id = this.getChatId();
 
-        Chats.insert({
-            title: messageToSend,
-
-        })
-
-        //Chats.update(this._id, { $set: { messages: messageToSend }});
-
-        console.log('Sending message: '+ messageToSend);
+        // Act
+        Chats.update(id, { $set: { messages: messageToSend }});
+        ReactDOM.findDOMNode(this.refs.messageDraft).value = '';
     }
 }
 
